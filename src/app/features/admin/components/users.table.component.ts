40fc741fee5ce69999fb8cf8b8e646bfc0dcea63
ggueho9 <ggueho@facyle.com>
2025-09-22 22:32:11 +0200

    feat(directive): ajout d'une directive highlight -> utilisé dans l'admin

--- src/app/features/admin/components/users.table.component.ts
+++ src/app/features/admin/components/users.table.component.ts
@@ -1,6 +1,7 @@
 import { User } from '@/auth/models/user.model';
 import { AuthService } from '@/auth/services/auth.service';
 import { GenericTableComponent } from '@/shared/components/ui/table.component';
+import { HighlightDirective } from '@/shared/directives/highlight.directive';
 import { RolesPipe } from '@/shared/pipes/roles.pipe';
 import { CommonModule } from '@angular/common';
 import { Component, inject, Input } from '@angular/core';
@@ -10,7 +11,14 @@ import { TableModule } from 'primeng/table';
 @Component({
   selector: 'app-admin-users-table',
   standalone: true,
-  imports: [TableModule, ButtonModule, RolesPipe, GenericTableComponent, CommonModule],
+  imports: [
+    TableModule,
+    ButtonModule,
+    RolesPipe,
+    GenericTableComponent,
+    CommonModule,
+    HighlightDirective,
+  ],
   template: `
     <app-generic-table
       [columns]="cols"
@@ -19,7 +27,14 @@ import { TableModule } from 'primeng/table';
     ></app-generic-table>
     <ng-template #userRowTpl let-user>
       <td style="text-align:center">{{ user.id }}</td>
-      <td style="text-align:center">{{ user.name }}</td>
+      <td style="text-align:center">
+        <div
+          [appHighlight]="user.id === currentUser()?.id ? 'rgba(100, 268, 68, 0.1)' : 'transparent'"
+          class="rounded-md"
+        >
+          {{ user.name }}
+        </div>
+      </td>
       <td style="text-align:center">{{ user.email }}</td>
       <td style="text-align:center">{{ user.role | roles }}</td>
       <td>
@@ -35,6 +50,8 @@ export class AdminUsersTableComponent {
   cols: string[] = ['ID', 'Nom', 'Email', 'Role', 'Action'];
 
   private authService = inject(AuthService);
+
+  currentUser = this.authService.currentUser$;
 
   deleteUser(userId: number) {
     // TODO:Ajouter une modal de confirmation
